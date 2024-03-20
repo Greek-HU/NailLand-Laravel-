f6d8bafa41d3b9e85cb55879c7701de9d06a1316
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-03-20 11:32:28 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::controller(PageController::class)->group(function(){
+    Route::get('dashboard', 'dashboard')->name('dashboard');
 });
