0dc92f464fa528bc61b721a6ad7cd857fb7ea2c5
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-03-23 15:24:59 +0100

    Update signUpForm.blade.php

--- resources/views/auth/signUpForm.blade.php
+++ resources/views/auth/signUpForm.blade.php
@@ -6,7 +6,8 @@
         <div class="container">
           <div class="row">
             <div class="col-6 d-lg-flex d-none h-100 my-auto pe-0 position-absolute top-0 start-0 text-center justify-content-center flex-column">
-              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center" style="background-image: url(' {{ asset('/img/illustrations/illustration-signup.jpg') }} '); background-size: cover;">
+              <div class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center" 
+              style="background-image: url(' {{ asset("img/illustrations/illustration-signup.jpg") }}'); background-size: cover;">
               </div>
             </div>
             <div class="col-xl-4 col-lg-5 col-md-7 d-flex flex-column ms-auto me-auto ms-lg-auto me-lg-5">
@@ -20,15 +21,15 @@
                     @csrf
                     <div class="input-group input-group-outline mb-3">
                       <label class="form-label"></label>
-                      <input type="text" class="form-control" placeholder="Name">
+                      <input type="text" class="form-control" name="name" placeholder="Name">
                     </div>
                     <div class="input-group input-group-outline mb-3">
                       <label class="form-label"></label>
-                      <input type="email" class="form-control" placeholder="Email">
+                      <input type="email" class="form-control" name="email" placeholder="Email">
                     </div>
                     <div class="input-group input-group-outline mb-3">
                       <label class="form-label"></label>
-                      <input type="password" class="form-control" placeholder="Password">
+                      <input type="password" class="form-control" name="password" placeholder="Password">
                     </div>
                     <div class="form-check form-check-info text-start ps-0">
                       <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault" checked>
@@ -37,7 +38,7 @@
                       </label>
                     </div>
                     <div class="text-center">
-                      <button type="button" class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">Sign Up</button>
+                      <button type="submit" class="btn btn-lg bg-gradient-primary btn-lg w-100 mt-4 mb-0">Sign Up</button>
                     </div>
                   </form>
                 </div>
