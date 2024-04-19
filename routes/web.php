03e968687436d2b9833b87dfb2e23fc1a40a8c79
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-19 12:14:02 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PublicController;
 use App\Http\Middleware\NameToUppercaseMiddleware;
+use App\Http\Middleware\LogInControll;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,9 +22,18 @@ Route::controller(PageController::class)->group(function()
 {
     Route::get('dashboard', 'dashboard')->name('dashboard');
     Route::get('sign-in', 'signInTpl')->name('sign-in');
-    Route::post('login', 'signInPost')->name('login.post');
-    Route::get('/', 'home')->name('home');
+    Route::post('login', 'signInPost')->name('login-post');
+    Route::get('logout', 'logout')->name('logout');
+    Route::get('private', 'home')->name('home')->middleware(LogInControll::class);
     Route::get('sign-up', 'create')->name('sign-up');
-    Route::post('/store', 'signUp')->name('store')->middleware(NameToUppercaseMiddleware::class);
+    Route::post('/store', 'signUp')->name('store');
+
+});
+
+Route::controller(PublicController::class)->group(function()
+{
+    Route::get('/', 'public')->name('public');
+    Route::get('service', 'service')->name('service');
+    Route::get('contact', 'contact')->name('contact');
 
 });
