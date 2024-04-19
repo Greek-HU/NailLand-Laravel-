16dbcfeff5a545b13cd20467607d3fcedce439ea
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-19 12:13:55 +0200

    Create contact.blade.php

---
+++ resources/views/public/contact.blade.php
@@ -0,0 +1,14 @@
+@extends('layoute.public')
+@extends('public.publicNavBar')
+@section('content')
+<div class="cont pt-4">
+    <div class="cont_box">
+        <h2>Időpont foglalás</h2>
+        <ul >
+            <li class="p-2"><strong>Cím: </strong><span>7700 Mohács E u. 8.</span></li>
+            <li class="p-2"><strong>Email: </strong><span>Email cím</span></li>
+            <li class="p-2 pb-3"><strong>Tel.: </strong><span>06 20 1234 567</span></li>
+        </ul>
+    </div>
+</div> 
+@endsection
