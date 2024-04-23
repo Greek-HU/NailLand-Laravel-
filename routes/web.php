5c30a3501fff63f62cadd2135678ad253f11170b
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-23 10:55:06 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\PageController;
 use App\Http\Controllers\PublicController;
+use App\Http\Controllers\PrivateController;
 use App\Http\Middleware\NameToUppercaseMiddleware;
 use App\Http\Middleware\LogInControll;
 use Illuminate\Support\Facades\Route;
@@ -27,6 +28,7 @@ Route::controller(PageController::class)->group(function()
     Route::get('private', 'home')->name('home')->middleware(LogInControll::class);
     Route::get('sign-up', 'create')->name('sign-up');
     Route::post('/store', 'signUp')->name('store');
+    Route::get('upload', 'upload')->name('upload');
 
 });
 
@@ -34,6 +36,14 @@ Route::controller(PublicController::class)->group(function()
 {
     Route::get('/', 'public')->name('public');
     Route::get('service', 'service')->name('service');
+    Route::get('gallery', 'gallery')->name('gallery');
     Route::get('contact', 'contact')->name('contact');
 
+});
+
+Route::controller(PrivateController::class)->group(function()
+{
+    Route::get('upload', 'upload')->name('upload');
+    Route::get('editContForm', 'editContact')->name('editContForm');
+    Route::put('updateContData', 'updateCont')->name('updateContData');
 });
