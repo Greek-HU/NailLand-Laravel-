40373988dfbcd981e4e81d659efa9e23d2718cf0
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-23 11:34:56 +0200

    Update PrivateController.php

--- app/Http/Controllers/PrivateController.php
+++ app/Http/Controllers/PrivateController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use App\Models\Service;
 use Illuminate\Http\Request;
 
 class PrivateController extends Controller
@@ -14,26 +15,34 @@ class PrivateController extends Controller
     {
         //
     }
+    public function editService()
+    {
+        $services = Service::all();
+        return view('private.editService');
+    }
     public function upload()
     {
         return view('partials.picUp');
     }
     public function editContact()
     {
-        $user = User::all();
+        $user = User::find(1);
         return view('private.editContact');
     }
     /**
      * Show the form for creating a new resource.
      */
-    public function updateCont(User $user, string $id)
+    public function updateCont(Request $request)
     {
-        $user = User::find($id);
-        $user->address = $request->input('address');
+        $user = User::find(1);
+        $user->email = $request->input('newEmail');
+        $user->phoneNum = $request->input('newPhoneNum');
+
+        $user->address = $request->input('newAddress');
 
         $user->update();
 
-        return redirect()->route('editContForm');
+        return redirect()->route('editContForm')->with('status','Student Updated Successfully');
     }
 
     /**
