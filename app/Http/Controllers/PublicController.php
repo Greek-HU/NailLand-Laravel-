1dfb322775da882b3a966ae7c1b662761b69efec
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-23 10:55:51 +0200

    Update PublicController.php

--- app/Http/Controllers/PublicController.php
+++ app/Http/Controllers/PublicController.php
@@ -29,9 +29,9 @@ class PublicController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function gallery()
     {
-        //
+        return view('public.gallery');
     }
 
     /**
