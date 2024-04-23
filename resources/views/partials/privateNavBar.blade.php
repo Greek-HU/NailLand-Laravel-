0d64d4d5929884be1feb10b4a8c38dbc8532433f
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-23 10:55:12 +0200

    Create privateNavBar.blade.php

---
+++ resources/views/partials/privateNavBar.blade.php
@@ -0,0 +1,21 @@
+@section('privateNavBar')
+<div class="navbar">
+        <div class="logo">
+            <img src="{{ asset('img/carousel/logo.jpg') }}" class="logo justify-content-start" alt="Körmös logó">
+        </div>
+
+        <div class="nav_links">
+
+            <a href="/" class="fst_pg">Kezdőlap</a>
+            <a href="service" class="services">Szolgáltatások</a>
+            <a href="gallery" class="gall_on">Galéria</a>
+            <a href="upload">Képfeltöltés</a>
+            <div class="d-flex justify-content-end">
+                <a href="editContForm" class="contact d-flex align-items-end justify-content-end">Elérhetőség</a>
+            </div>
+        </div>
+        <a href="javascript:void(0);" class="icon text-center" onclick="mobil_navbar()">
+            <i class="fa fa-bars"></i>
+          </a>
+    </div>
+@endsection
