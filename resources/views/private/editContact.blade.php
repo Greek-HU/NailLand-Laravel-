aec34692fe2dc29ae722bfdb89755db940edc7db
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-23 11:35:07 +0200

    Update editContact.blade.php

--- resources/views/private/editContact.blade.php
+++ resources/views/private/editContact.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 <div class="cont pt-4">
     <div class="cont_box">
+            @if (session('status'))
+                <h6 class="alert alert-success">{{ session('status') }}</h6>
+            @endif
         <h2>Időpont foglalás</h2>
         <form action="updateContData" method="post">
             @csrf
@@ -10,18 +13,19 @@
         <ul >
             <li class="p-2">
                 <strong>Cím: </strong>
-                <input type="text" name="address" id="" placeholder="{{ auth()->user()->address }}">
-                <button type="submit">Mentés</button>
-                
+                <input type="text" name="newAddress" id="" 
+                value="{{ auth()->user()->address }}">
             </li>
             <li class="p-2">
                 <strong>Email: </strong>
-                <span>Email cím</span>
-            </li>
+                <input type="email" name="newEmail" id="" 
+                value="{{ auth()->user()->email }}">            </li>
             <li class="p-2 pb-3">
                 <strong>Tel.: </strong>
-                <span>06 20 1234 567</span>
+                <input type="tel" name="newPhoneNum" id="" 
+                value="{{ auth()->user()->phoneNum }}">
             </li>
+            <button type="submit">Mentés</button>
         </ul>
         </form>
     </div>
