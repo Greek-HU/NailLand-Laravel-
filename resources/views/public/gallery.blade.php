eeaaf6514cf8f93158570fad7ced95eb1e362771
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-23 10:55:48 +0200

    Create gallery.blade.php

---
+++ resources/views/public/gallery.blade.php
@@ -0,0 +1,16 @@
+@extends('layoute.public')
+@extends('public.publicNavBar')
+@section('content')
+<div class="d-flex justify-content-center">
+    <div class="op_img">
+    </div>
+</div>
+
+<div class="container-fluid">
+    <div class="row">
+        <div class="gallery col-12 col-md-8">
+
+        </div>
+    </div>
+</div>
+@endsection
