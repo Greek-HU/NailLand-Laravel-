553d3934af669b20e9f50682e8ea501d2e889cb4
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-23 10:55:33 +0200

    Create 2024_04_22_081529_create_pictures_table.php

---
+++ database/migrations/2024_04_22_081529_create_pictures_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pictures', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 128);
+            $table->string('pic_type', 10);
+            $table->longText('pic_data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pictures');
+    }
+};
