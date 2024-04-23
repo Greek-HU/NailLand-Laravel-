273a79bb7731785ed3d20e4a7245c61abccebc7e
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-23 10:55:46 +0200

    Create Pictures.php

---
+++ app/Models/Pictures.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pictures extends Model
+{
+    protected $table = 'pictures';
+    protected $fillable = [
+        'title',
+        'pic_type',
+        'pic_data',
+    ];
+}
