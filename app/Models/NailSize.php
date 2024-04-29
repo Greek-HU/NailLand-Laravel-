f25655cae087607b2997fd13c3b84f646b062350
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-29 09:24:11 +0200

    Create NailSize.php

---
+++ app/Models/NailSize.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class NailSize extends Model
+{
+    protected $table = 'nail_size';
+    protected $fillable = [
+        'size_name',
+    ];
+
+    public function NailType(): BelongsTo
+    {
+        return $this>BelongsTo(NailType::class);
+    }
+
+    public function Price(): BelongsTo
+    {
+        return $this>BelongsTo(Price::class);
+    }
+}
