bf04c4d5847c02c8959da411fd51147bf0f11f1d
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-29 12:22:10 +0200

    Update private.blade.php

--- resources/views/layoute/private.blade.php
+++ resources/views/layoute/private.blade.php
@@ -8,6 +8,7 @@
     <meta name="keywords" content="köröm, nails, körömfestés, körömlakk, géllakk gél lakk">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/public.css') }}">
     <link rel="stylesheet" href="{{ asset('css/carousel.css') }}">
