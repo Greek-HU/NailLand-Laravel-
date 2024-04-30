2c44b8ed277f8e028eec6c935a9688a80a4c3c39
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-04-30 12:32:11 +0200

    Update NailSize.php

--- app/Models/NailSize.php
+++ app/Models/NailSize.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class NailSize extends Model
 {
@@ -12,13 +13,13 @@ class NailSize extends Model
         'size_name',
     ];
 
-    public function NailType(): BelongsTo
+    public function NailType() : BelongsTo
     {
-        return $this>BelongsTo(NailType::class);
+        return $this->BelongsTo(NailType::class);
     }
 
-    public function Price(): BelongsTo
+    public function Price() : BelongsTo
     {
-        return $this>BelongsTo(Price::class);
+        return $this->BelongsTo(Price::class);
     }
 }
