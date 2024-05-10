900a3494beb377e45293746bdfc6cef54270b887
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-05-10 11:37:29 +0200

    Update 2024_04_26_085559_create_nail_size_table.php

--- database/migrations/2024_04_26_085559_create_nail_size_table.php
+++ database/migrations/2024_04_26_085559_create_nail_size_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('nail_size', function (Blueprint $table) {
             $table->id();
             $table->string('size_name');
+            
             $table->timestamps();
         });
     }
