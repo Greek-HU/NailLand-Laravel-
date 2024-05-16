9e4bce038c57d942735729b270c40e4426a04f30
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-05-16 16:05:40 +0200

    Update picUp.blade.php

--- resources/views/private/picUp.blade.php
+++ resources/views/private/picUp.blade.php
@@ -1,5 +1,6 @@
 @extends('layoute.private')
 @extends('partials.privateNavBar')
+
 @section('content')
 <div class="box pb-4">
     <div class="text box_color">
@@ -27,9 +28,23 @@
             <div class="row">
                 <div class="gallery col-12 col-md-8">
                     @foreach ($pictures as $picture)
-                    <p>{{$picture->title}}</p>
-                    <img src="{{$picture->imgPlace}}" alt="" height="250">
+                    <div class="gallery__column">
+                        <div class="gallery__link">
+                            <figure class="gallery__thumb">
+                                <img src="{{$picture->imgPlace}}" onclick="img_box(this)" alt="Portrait by Jessica Felicio" height="250" class="gallery__image">
+                                <figcaption class="gallery__caption">{{$picture->title}}</figcaption>
+                            </figure>
+                            <!-- Modal Button -->
+                            <div class="d-flex justify-content-center">
+                                    <a class="btn btn-success" href="editPic/picID={{$picture->id}}" data-bs-toggle="modal" data-bs-target="#editModal">Szerkesztés</a>
 
+                                
+                                <a  class="btn btn-danger ms-2" href="#" data-bs-toggle="modal" data-bs-target="#xModal" data-pic-id="{{$picture->id}}" onclick="img_box(this)">
+                                    X
+                                </a>
+                            </div>
+                        </div>
+                    </div>
                     @endforeach
                 </div>
             </div>
@@ -37,5 +52,26 @@
     </div>
 </div>
 </div>
-
+<!--Delete Modal ->
+@foreach ($pictures as $picture)
+<form action="destroyPic/picID=" method="post">
+<div class="modal fade" id="xModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Mégsem</button>
+                <button type="submit" class="btn btn-primary">Törlés</button>
+            </div>
+        </div>
+    </div>
+</div>
+</form>
+@endforeach-->
 @endsection
+@extends('private.xmodal')
