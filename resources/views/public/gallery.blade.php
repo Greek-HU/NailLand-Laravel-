51796aaa514a47d959a8bde8bfd8f8ec050c9dad
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-05-16 16:05:34 +0200

    Update gallery.blade.php

--- resources/views/public/gallery.blade.php
+++ resources/views/public/gallery.blade.php
@@ -9,7 +9,16 @@
 <div class="container-fluid">
     <div class="row">
         <div class="gallery col-12 col-md-8">
-
+            @foreach ($pictures as $picture)
+            <div class="gallery__column">
+                <div class="gallery__link">
+                    <figure class="gallery__thumb">
+                        <img src="{{$picture->imgPlace}}" onclick="img_box(this)" alt="Portrait by Jessica Felicio" height="250" class="gallery__image">
+                        <figcaption class="gallery__caption">{{$picture->title}}</figcaption>
+                    </figure>
+                </div>
+            </div>
+            @endforeach
         </div>
     </div>
 </div>
