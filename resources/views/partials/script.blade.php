60cc5feec0efffca4fabfada704cba591b6bbd96
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-05-29 10:17:38 +0200

    Create script.blade.php

---
+++ resources/views/partials/script.blade.php
@@ -0,0 +1,21 @@
+@section('script')
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+
+<script>
+   
+    $(document).ready(function () {
+        $('.delbtn').click(function(){
+            var img_id = $(this).val();
+            alert($(this).val());
+        });
+    });
+    $(document).ready(function () {
+        $(document).on('click', '.editbtn', function(){
+            var img_id = $(this).val();
+            //alert(img_id);
+            console.log('alma');
+        });
+    });
+</script>
+@endsection
