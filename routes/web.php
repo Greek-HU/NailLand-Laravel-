f435f80afd27b45d47fbfdc1ebad03040c9c1670
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-05-29 10:17:41 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller(PageController::class)->group(function()
 {
-    Route::get('dashboard', 'dashboard')->name('dashboard');
+    Route::get('logout', 'logOut')->name('logout');
     Route::get('sign-in', 'signInTpl')->name('sign-in');
     Route::post('login', 'signInPost')->name('login-post');
     Route::get('logout', 'logout')->name('logout');
@@ -43,9 +43,11 @@ Route::controller(PrivateController::class)->middleware(LogInMiddleware::class)-
     // Típus szerkesztés
     Route::get('editService', 'editService')->name('editService');
     Route::put('updateType', 'updateType')->name('updateType');
+
     // Méret és Ár szerkesztése
     Route::get('editNailData/nailID={id}', 'editNail')->name('editNail');
     Route::put('editNailData/updateNail/sizeID={id}', 'updateNail')->name('updateNail/sizeID={id}');
+
     // Kapcsolatok szerkesztés
     Route::get('editContForm', 'editContact')->name('editContForm');
     Route::put('updateContData', 'updateCont')->name('updateContData');
@@ -56,11 +58,13 @@ Route::controller(GalleryController::class)->middleware(LogInMiddleware::class)-
     // Képfeltöltés
     Route::get('picUploader', 'index')->name('picUploader');
     Route::post('storeImage', 'create')->name('storeImage');
-    //Kép Szeresztés
-    Route::get('editPic/picID={id}', 'edit')->name('editPic');
 
-    Route::post('updatePic/picID={id}', 'update')->name('updatePic');
+    //Kép Szeresztés
+    Route::get('editPic/{id}', 'edit')->name('editPic');
+    Route::put('updatePic', 'update')->name('updatePic');
+
     // Kép törlése
-    Route::get('destroyPic/picID={id}', 'destroy')->name('destroyPic');
+    Route::get('quest/{id}', 'questDestroy')->name('questPic={id}');
+    Route::get('destroyPic', 'destroy')->name('destroyPic');
 
 });
