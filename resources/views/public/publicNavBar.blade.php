1fdd27119cc477ebd3a2ed51869d5bc0cda851e6
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-05-29 10:17:21 +0200

    Update publicNavBar.blade.php

--- resources/views/public/publicNavBar.blade.php
+++ resources/views/public/publicNavBar.blade.php
@@ -9,8 +9,14 @@
             <a href="/" class="fst_pg">Kezdőlap</a>
             <a href="service" class="services">Szolgáltatások</a>
             <a href="gallery" class="gall_on">Galéria</a>
+            @if(auth()->user() != null)
+                <a href="{{route('picUploader')}}">Képfeltöltés</a>
+            @endif
             <div class="d-flex justify-content-end">
                 <a href="contact" class="contact d-flex align-items-end justify-content-end">Elérhetőség</a>
+                @if(auth()->user() != null)
+                    <a href="{{route('logout')}}">Kijelentkezés</a>
+                @endif
             </div>
         </div>
         <a href="javascript:void(0);" class="icon text-center" onclick="mobil_navbar()">
