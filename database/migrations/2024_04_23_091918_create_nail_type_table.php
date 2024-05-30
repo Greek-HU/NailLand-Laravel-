59eecabf55e0cb8afc67536fc66bc001078efe5b
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-05-30 18:06:26 +0200

    Update 2024_04_23_091918_create_nail_type_table.php

--- database/migrations/2024_04_23_091918_create_nail_type_table.php
+++ database/migrations/2024_04_23_091918_create_nail_type_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('nail_type', function (Blueprint $table) {
             $table->id();
             $table->string('type');
+            $table->string('size_title');
+            $table->string('price_title');
             $table->timestamps();
         });
     }
