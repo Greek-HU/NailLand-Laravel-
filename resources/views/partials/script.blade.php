a239cf29054db4a28497964a1665eb6b03241808
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-10-28 15:05:01 +0100

    Update script.blade.php

--- resources/views/partials/script.blade.php
+++ resources/views/partials/script.blade.php
@@ -1,24 +1,84 @@
-@section('script')
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/js/bootstrap.bundle.min.js" integrity="sha512-7Pi/otdlbbCR+LnW+F7PwFcSDJOuUJB3OxtEHbg4vSMvzvJjde4Po1v4BR9Gdc9aXNUNFVUY+SK51wWT8WF0Gg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-
 <script>
-   
-    $(document).ready(function () {
-        $('.delbtn').click(function(){
+    $(document).ready(function() {
+        $('.delbtn').click(function() {
             var img_id = $(this).val();
             alert($(this).val());
         });
     });
-    $(document).ready(function () {
-        $(document).on('click', '.editbtn', function(){
+    $(document).ready(function() {
+        $(document).on('click', '.editbtn', function() {
             var img_id = $(this).val();
             //alert(img_id);
             console.log('alma');
         });
     });
-    
+    $(document).ready(function() {
+        $(document).on('click', '.mobil_navbar', function() {
+            var nav_a = $s(".nav_links");
+            if (nav_a.style.display === "block") {
+                nav_a.style.display = "none";
+            } else {
+                nav_a.style.display = "block"
+            }
+        });
+    });
+    $(document).ready(function() {
+        $(document).on('click', '.showTypeInput', function() {
+            
+            var typeInput = document.querySelector(".typeInput");
+            var nailType = document.querySelector(".nailType");
+            if (typeInput.style.display === "table-cell") {
+                typeInput.style.display = "none";
+                nailType.style.display = "table-cell"
+            } else {
+                typeInput.style.display = "table-cell"
+                nailType.style.display = "none"
+            }
+            
+        });
+    });
+    $(document).ready(function() {
+        $(document).on('click', '.showPriceInput', function() {
+            
+            var priceInput = document.querySelector(".priceInput");
+            var sizePrice = document.querySelector(".sizePrice");
+            if (priceInput.style.display === "block") {
+                priceInput.style.display = "none";
+                sizePrice.style.display = "block"
+            } else {
+                priceInput.style.display = "block"
+                sizePrice.style.display = "none"
+            }
+            
+        });
+    });
+    $(document).ready(function() {
+        $(document).on('click', '.newBoxBtn', function() {
+            $('#addBox').modal('show');
+
+            $.ajax({
+                type: "GET",
+                url: "/addnewBox",
+                success: function(response) {
+                    $('#newNailType').text(response.nailType.type);
+                    $('#newNailSize').val(response.nailType.size_title);
+                    $('#newNailPrice').text(response.nailType.price_title);
+                }
+            });
+        });
+        $(document).on('click', '.newElementBtn', function() {
+            $('#addElement').modal('show');
+
+            $.ajax({
+                type: "GET",
+                url: "/addNewElement",
+                success: function(response) {
+                    $('#newType').val(response.nailType.id);
+                    
+                    $('#newNailSize').val(response.nailSize.id);
+                    $('#newAmount').text(response.Price.id);
+                }
+            });
+        });
+    });
 </script>
-@endsection
