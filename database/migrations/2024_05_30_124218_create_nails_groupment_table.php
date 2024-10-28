8706a60fb6a3ba9280c8d8f1a2bf463a37f0b73b
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-10-28 15:01:31 +0100

    Create 2024_05_30_124218_create_nails_groupment_table.php

---
+++ database/migrations/2024_05_30_124218_create_nails_groupment_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('nails_groupment', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('type_id');
+            $table->foreign('type_id')->references('id')->on('nail_type');
+            $table->unsignedBigInteger('opp_id');
+            $table->foreign('opp_id')->references('id')->on('nail_opportunities');
+            $table->unsignedBigInteger('size_id')->nullable();
+            $table->foreign('size_id')->references('id')->on('sizes');
+            $table->unsignedBigInteger('price_id');
+            $table->foreign('price_id')->references('id')->on('prices');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('nails_groupment');
+    }
+};
