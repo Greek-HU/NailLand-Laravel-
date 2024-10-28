37447682a2728c91b8fcfa8e8347a7f290b5de32
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-10-28 14:59:44 +0100

    Create NailGroupment.php

---
+++ app/Models/NailGroupment.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class NailGroupment extends Model
+{
+    use HasFactory;
+    protected $table = 'nails_groupment';
+    protected $fillable = [
+        'type_id',
+        'opp_id',
+        'size_id',
+        'price_id'
+    ];
+
+    public function EyeTypes() :  BelongsToMany
+    {
+        return $this->belongsToMany(NailType::class, 'nails_groupment', 'type_id');
+    }
+
+    public function EyeOpps() : BelongsToMany
+    {
+        return $this->belongsToMany(EyeOpp::class);
+    }
+
+    public function Sizes() : BelongsToMany
+    {
+        return $this->belongsToMany(Sizes::class);
+    }
+
+    public function Prices() : BelongsToMany
+    {
+        return $this->belongsToMany(Prices::class);
+    }
+
+}
