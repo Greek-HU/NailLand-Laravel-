3ce086eef61ec3b504f2c2b7e4f00a189d541e64
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-10-28 15:04:54 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,23 @@
 <?php
 
-use App\Http\Controllers\GalleryController;
-use App\Http\Controllers\PageController;
+use App\Http\Controllers\UserController;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\PrivateController;
-use App\Http\Middleware\NameToUppercaseMiddleware;
 use App\Http\Middleware\LogInMiddleware;
 use Illuminate\Support\Facades\Route;
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
-Route::controller(PageController::class)->group(function()
+Route::controller(UserController::class)->group(function()
 {
     Route::get('logout', 'logOut')->name('logout');
     Route::get('sign-in', 'signInTpl')->name('sign-in');
@@ -18,11 +26,13 @@ Route::controller(PageController::class)->group(function()
     Route::get('sign-up', 'create')->name('sign-up');
     Route::post('/store', 'signUp')->name('store');
 });
-
 Route::controller(PublicController::class)->group(function()
 {
     Route::get('/', 'public')->name('public');
-    Route::get('service', 'service')->name('service');
+    Route::post('addComment', 'createComment')->name('comment');
+    Route::get('destroyComm/{id}', 'destroyComment')->name('destroyComm');
+    Route::get('nails', 'nails')->name('nails');
+    Route::get('eyelashes', 'eyelashes')->name('eyelashes');
     Route::get('gallery', 'gallery')->name('gallery');
     Route::get('contact', 'contact')->name('contact');
 });
@@ -31,7 +41,7 @@ Route::controller(PrivateController::class)->middleware(LogInMiddleware::class)-
 {
     // Típus szerkesztés
     Route::get('editService', 'editService')->name('editService');
-    Route::put('updateType', 'updateType')->name('updateType');
+    Route::put('updateType={type_id}', 'updateType')->name('updateType');
 
     // Méret és Ár szerkesztése
     Route::get('editNailData/nailID={id}', 'editNail')->name('editNail');
@@ -44,23 +54,11 @@ Route::controller(PrivateController::class)->middleware(LogInMiddleware::class)-
     //Doboz törlése
     Route::get('typeDestroy/{id}', 'destroy')->name('typeDestroy');
 
+    //Elem létrehozás
+    Route::get('addNewElement', 'addElement')->name('addNewElement');
+    Route::post('createNewElement', 'createElement')->name('createNewBox');
+
     // Kapcsolatok szerkesztés
     Route::get('editContForm', 'editContact')->name('editContForm');
     Route::put('updateContData', 'updateCont')->name('updateContData');
-});
-
-Route::controller(GalleryController::class)->middleware(LogInMiddleware::class)->group(function()
-{
-    // Képfeltöltés
-    Route::get('picUploader', 'index')->name('picUploader');
-    Route::post('storeImage', 'create')->name('storeImage');
-
-    //Kép Szeresztés
-    Route::get('editPic/{id}', 'edit')->name('editPic');
-    Route::put('updatePic', 'update')->name('updatePic');
-
-    // Kép törlése
-    Route::get('quest/{id}', 'questDestroy')->name('questPic={id}');
-    Route::get('destroyPic', 'destroy')->name('destroyPic');
-
 });
