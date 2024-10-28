a500d88f1c620c3c2e635e2cbf85cbd4bf585edf
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-10-28 15:04:48 +0100

    Update app.blade.php

--- resources/views/layoute/app.blade.php
+++ resources/views/layoute/app.blade.php
@@ -7,9 +7,9 @@
 <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('img/apple-icon.png') }}">
 <link rel="icon" type="image/png" href="{{ asset('img/favicon.png') }}">
 
-<title>NailsLand</title>
+<title>Adele nail</title>
 <!--     Fonts and icons     -->
-<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <!-- Nucleo Icons -->
 <link href="{{ asset('css/nucleo-icons.css') }}" rel="stylesheet" />
