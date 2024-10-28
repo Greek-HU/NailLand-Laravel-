3ed0e180983153dff10e5e3e041b6beae4bc68d6
Greek-HU <101356147+Greek-HU@users.noreply.github.com>
2024-10-28 15:04:27 +0100

    Update signIn.blade.php

--- resources/views/auth/signIn.blade.php
+++ resources/views/auth/signIn.blade.php
@@ -9,29 +9,28 @@
 </div>  
 @endif
 <main class="main-content  mt-0">
-    <div class="page-header align-items-start min-vh-100" style="background-image: url('https://images.unsplash.com/photo-1497294815431-9365093b7331?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1950&q=80');">
+    <div class="page-header align-items-start min-vh-100" style="background-image: url(' {{ asset("img/logIn.jpeg") }}' )">
       <span class="mask bg-gradient-dark opacity-6"></span>
       <div class="container my-auto">
         <div class="row">
           <div class="col-lg-4 col-md-8 col-12 mx-auto">
             <div class="card z-index-0 fadeIn3 fadeInBottom">
               <div class="card-header p-0 position-relative mt-n4 mx-3 z-index-2">
-                <div class="bg-gradient-primary shadow-primary border-radius-lg py-3 pe-1">
+                <div class=" shadow-primary border-radius-lg py-3 pe-1">
                   <h4 class="text-white font-weight-bolder text-center mt-2 mb-0">Belépés</h4>
                   <div class="row mt-3">
                     <div class="col-2 text-center ms-auto">
-                      <a class="btn btn-link px-3" href="javascript:;">
+                      <a class="btn btn-link px-3" href="https://www.facebook.com/adele95.nails" target="_blank">
                         <i class="fa fa-facebook text-white text-lg"></i>
                       </a>
-                    </div>
-                    <div class="col-2 text-center px-1">
-                      <a class="btn btn-link px-3" href="javascript:;">
-                        <i class="fa fa-github text-white text-lg"></i>
+                    </div><div class="col-2 text-center px-1">
+                    <a class="btn btn-link px-3" href="https://www.instagram.com/adelenails_" target="_blank">
+                        <i class="fa fa-instagram text-white text-lg"></i>
                       </a>
                     </div>
                     <div class="col-2 text-center me-auto">
-                      <a class="btn btn-link px-3" href="javascript:;">
-                        <i class="fa fa-google text-white text-lg"></i>
+                      <a class="btn px-3" href="https://www.tiktok.com/@adelenails">
+                        <i class="fa-brands fa-tiktok text-white text-lg"></i>
                       </a>
                     </div>
                   </div>
@@ -41,24 +40,22 @@
                 <form role="form" action="{{ route('login-post') }}" method="POST" class="text-start">
                 @csrf
                   <div class="input-group input-group-outline my-3">
-                    <label class="form-label">Email</label>
-                    <input type="email" class="form-control" name="email">
+                    <input type="email" class="form-control" name="email" placeholder="Email">
                   </div>
                   <div class="input-group input-group-outline mb-3">
-                    <label class="form-label">Jelszó</label>
-                    <input type="password" class="form-control" name="password">
+                    <input type="password" class="form-control" name="password" placeholder="Jelszó">
                   </div>
                   <!--div class="form-check form-switch d-flex align-items-center mb-3">
                     <input class="form-check-input" type="checkbox" id="rememberMe" checked>
                     <label class="form-check-label mb-0 ms-3" for="rememberMe">Remember me</label>
                   </div-->
                   <div class="text-center">
-                    <button type="submit" class="btn bg-gradient-primary w-100 my-4 mb-2">Belépés</button>
+                    <button type="submit" class="btn btn-color w-100 my-4 mb-2">Belépés</button>
                   </div>
-                  <p class="mt-4 text-sm text-center">
+                  <!--p class="mt-4 text-sm text-center">
                     Nem rendelkezik még fiókkal?
                     <a href="/sign-up" class="text-primary text-gradient font-weight-bold">Regisztráció</a>
-                  </p>
+                  </p-->
                 </form>
               </div>
             </div>
